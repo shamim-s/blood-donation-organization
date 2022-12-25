bbc14e35a19383b5e071186cddc20b393b6eabe6
unknown <shamimibas@gmail.com>
2022-12-25 21:52:25 +0600

    in all blood groups section added dynamic data

--- src/Pages/Home/BloodCategory.js
+++ src/Pages/Home/BloodCategory.js
@@ -1,34 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const BloodCategory = () => {
+  const { data: bloodCategory = [], refetch} = useQuery({
+    queryKey: ['bloodCategory'],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/category`);
+      const data = res.json();
+      return data;
+    }
+  });
+  console.log(bloodCategory);
   return (
     <div className="mt-10 mb-10 ">
-        <span className="w-5 h-5 border-l-8 border-primary mr-2"></span><span className="text-2xl font-bold text-primary mb-5">All Blood Grups</span>
-      <div className="grid lg:grid-cols-8">
-        <div className="mx-auto w-14 h-14 rounded-lg bg-primary flex justify-center items-center">
-          <h1 className="text-2xl font-semibold text-white">A+</h1>
-        </div>
-        <div className="mx-auto w-14 h-14 rounded-lg bg-primary flex justify-center items-center">
-          <h1 className="text-2xl font-semibold text-white">O+</h1>
-        </div>
-        <div className="mx-auto w-14 h-14 rounded-lg bg-primary flex justify-center items-center">
-          <h1 className="text-2xl font-semibold text-white">B+</h1>
-        </div>
-        <div className="mx-auto w-14 h-14 rounded-lg bg-primary flex justify-center items-center">
-          <h1 className="text-2xl font-semibold text-white">AB+</h1>
-        </div>
-        <div className="mx-auto w-14 h-14 rounded-lg bg-primary flex justify-center items-center">
-          <h1 className="text-2xl font-semibold text-white">A-</h1>
-        </div>
-        <div className="mx-auto w-14 h-14 rounded-lg bg-primary flex justify-center items-center">
-          <h1 className="text-2xl font-semibold text-white">O-</h1>
-        </div>
-        <div className="mx-auto w-14 h-14 rounded-lg bg-primary flex justify-center items-center">
-          <h1 className="text-2xl font-semibold text-white">B-</h1>
-        </div>
-        <div className="mx-auto w-14 h-14 rounded-lg bg-primary flex justify-center items-center">
-          <h1 className="text-2xl font-semibold text-white">AB-</h1>
-        </div>
+        <span className="w-5 h-5 border-l-8 border-primary mr-2 lg:ml-0 ml-2"></span>
+        <span className="text-2xl font-bold text-primary lg:ml-0 ml-2">All Blood Groups</span>
+      <div className="grid lg:grid-cols-8 mt-5 grid-cols-4 gap-4">
+        {
+          bloodCategory.map(blood => <div 
+          key={blood._id}
+          className="mx-auto w-16 h-16 bg-primary rounded-lg flex justify-center items-center"
+          >
+            <h1 className="text-2xl text-white font-semibold">{blood.bloodGroup}</h1>
+          </div>)
+        }
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Context from "./Context/Context";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <Context>
       <App />
       <Toaster/>
     </Context>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
